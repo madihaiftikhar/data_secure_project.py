dcb3b7129e364ba9c4520c0ef9e5b073db9279d2
madihaiftikhar <164077906+madihaiftikhar@users.noreply.github.com>
2025-04-28 22:52:01 +0500

    first commit

---
+++ datasecure.py
@@ -0,0 +1,164 @@
+#Develope a Stremlit based secure data storage and retrievel system
+
+
+import streamlit as st
+import hashlib
+import json
+import os
+import time
+from cryptography.fernet import Fernet
+from base64 import urlsafe_b64encode
+from hashlib import pbkdf2_hmac
+
+
+# ===Data INformation of User ====
+DATA_FILE = "secure_data.json"
+SALT = b"secure_salt_value"
+LOCKOUT_DURATION = 60 
+
+
+# === Section Login Details
+
+if "authenticated_user" not in st.session_state:
+    st.session_state.authenticated_user =None
+
+if "failed_attempts" not in st.session_state:
+    st.session_state.failed_attempts = 0
+
+if "lockout_time" not in st.session_state:
+    st.session_state.lockout_time = 0
+
+# ===If Data is load ===
+def load_data():        
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE, "r") as f:
+            return json.load(f)
+    return{}
+
+def save_data(data):
+    with open(DATA_FILE, "w") as f:
+        json.dump(data, f)
+
+def generate_key(passkey):
+    key = pbkdf2_hmac('sha256' , passkey.encode(), SALT, 100000)
+    return urlsafe_b64encode(key)
+
+def hash_password(password):   
+    return hashlib.pbkdf2_hmac('sha256',password.encode(), SALT, 100000).hex()
+
+
+# === Cryptography.Fernet used ===
+def encrypt_text(text, key):
+    cipher = Fernet(generate_key(key))
+    return cipher.encrypt(text.encode()).decode()
+
+def decrypt_text(encrypt_text, key):
+    try:
+      cipher = Fernet(generate_key(key))
+      return cipher.decrypt(encrypt_text.encode()).decode()
+    except:
+          return None
+       
+stored_data = load_data()
+
+# === Navigation Bar ===
+st.title("🔐 Secure Data Encryption System")
+menu = ["Home" ,"Register" ,"Login" ,"Store Data" ,"Retieve Data"]
+choice = st.sidebar.selectbox("Navigation" ,menu)
+
+if choice == "Home":
+    st.subheader(" Welcome To My 🔐 Data Encryption System Using Streamlit")
+    st.markdown ("Develope a Streamlit -based secure data storage and retrievel system where:Users store data with a unique passkey.Users decrypt data by providing the correct passkey .Multiple failed attempts result in a forced reauthorization (login page).The system operates entirely in memory without external databases ")
+
+# ===User Registration ===
+elif choice == "Register":
+    st.subheader("✏️ Register new user")
+    username = st.text_input("Choose username")
+    password = st.text_input("Choose password" , type="password")
+
+    if st.button("Register"):
+        if username and password:
+           if username in stored_data:
+              st.warning("⚠️ User already exists.")
+           else:
+               stored_data[username ] = {
+                   "password": hash_password(password),
+                   "data" : []
+                }
+               save_data(stored_data)
+               st.success("✔️ User register successfully!")
+        else:
+             st.error("Both fields are required.")
+           
+    elif choice == "Login":
+        st.subheader("🔑User Login")
+
+        if time.time() < st.session_state.lockout_time:
+            remaining = int(st.session_state.lockout_time - time.time())
+            st.error(f"⏱️ Too many failed attempts.please wait {remaining} seconds.")
+            st.stop()
+
+        username = st.text_input("username")
+        password = st.text_input("password", type="password")
+
+        if st.button("login"):
+            if username in stored_data and stored_data[username]["password"] == hash_password(password):
+                st.session_state.authenticated_user = username
+                st.session_state.failed_attempts = 0
+                st.success(f"✅ Welcome {username}")
+            else:
+                st.session_state.failed_attempts += 1
+                remaining = 3 - st.session_state.failed_attempts
+                st.error(f"❌Invalid Crendentials !Attempts left: {remaining}")
+
+                if st.session_state.failed_attempts >= 3:
+                    st.session_state.lockout_time = time.time() + LOCKOUT_DURATION
+                    st.error("🛑 To many failed attempts. Locked for 60 seconds ")
+                    st.stop()
+
+# === Store Data Section ===
+elif choice == "Store Data":
+     if not st.session_state.authenticated_user:
+        st.warning("🔐Please login first.") 
+     else:
+         st.subheader("📦Store Encrypted Data")
+         data = st.text_area('Enter data to encrypt')
+         passkey = st.text_input("Encryption key (passphrase)", type="password")
+
+         if st.button("Encrypt And Save"):
+             if data and passkey:
+                 encrypted = encrypt_text(data, passkey)
+                 stored_data[st.session_state.authenticated_user]["data"].append(encrypted)
+                 save_data(stored_data)
+                 st.success("Data encrypted and save successfully!")
+
+             else:
+                st.error("All fields are required to fill.")
+
+# === Data Retrieve section ====
+elif choice == "Retrieve Data":
+    if not st.session_state .unauthenticated_user:
+        st.warning("Please login first")
+    else:
+       st.subheader("Reterive Data")
+       user_data =stored_data.get(st.session_state.unauthenticated_user, {}).get("data", [])
+       
+       if not user_data:
+        st.info("No Data Found!")
+       else:
+            st.write("Encrypted Data Enteries:")
+            for i,item in enumerate(user_data):
+                st.code(item,language="text")
+            
+            encrypted_input = st.text_area("Enter Encrypted Text")
+            passkey = st.text_input("Enter Passkey T Decrypt", type="password")
+
+            if st.button("Decrypt"):
+                result = decrypt_text(encrypted_input, passkey)
+                if result:
+                    st.success(f"Decrypted : {result}")
+                else:
+                     st.error("Incorrect passkey or corrupted data.")
+
+
+#if "authenticated_user" not in st.session_state:
