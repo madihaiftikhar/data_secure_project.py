1ea1a8212b6c32ef47b80507ad139ff6604d1624
madihaiftikhar <164077906+madihaiftikhar@users.noreply.github.com>
2025-04-30 23:49:51 +0500

    Updated encryption logic

--- data_secure.py
+++ data_secure.py
@@ -6,7 +6,7 @@ import hashlib
 import json
 import os
 import time
-from cryptography.fernet import Fernet
+from cryptography.fernet import Fernet # type: ignore
 from base64 import urlsafe_b64encode
 from hashlib import pbkdf2_hmac
 
@@ -160,5 +160,3 @@ elif choice == "Retrieve Data":
                 else:
                      st.error("Incorrect passkey or corrupted data.")
 
-
-#if "authenticated_user" not in st.session_state:
