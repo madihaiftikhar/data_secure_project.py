0e911898700944977118d7508936a6536a4fa9c9
madihaiftikhar <164077906+madihaiftikhar@users.noreply.github.com>
2025-04-30 23:54:20 +0500

    Updated encryption logic

--- data_secure.py
+++ data_secure.py
@@ -6,7 +6,7 @@ import hashlib
 import json
 import os
 import time
-from cryptography.fernet import Fernet # type: ignore
+from cryptography.fernet import Fernet 
 from base64 import urlsafe_b64encode
 from hashlib import pbkdf2_hmac
 
